7ed44a751f4659aaf5d22dcd67f74b578b36f69d
judah <87juda@gmail.com>
2022-03-30 16:56:10 -0300

    implement string size test

--- sum.js
+++ sum.js
@@ -1,4 +1,26 @@
-function sum(a, b) {
-  return a + b;
+// function sum(a, b) {
+//   return a + b;
+// }
+// module.exports = sum;
+
+function strSize(str) {
+  return str.length;
 }
-module.exports = sum;
+
+function testStrSize(mockStr) {
+  const mockStrSize = strSize(mockStr);
+
+  if (mockStrSize < 1) {
+    return 'It has less than 1 char';
+  } else if (mockStrSize > 10) {
+    return 'It has more than 10 chars';
+  } else {
+    return 'It has ' + mockStrSize + ' chars';
+  }
+}
+
+const test1 = '';
+const test2 = '7 chars';
+const test3 = 'I have 16 chars';
+
+console.log(testStrSize(test1));
